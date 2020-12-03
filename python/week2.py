cb785ca9ceac1d874d371a8028faf98d17a3ad30
Rafał Łojek <rafal.lojek@gmail.com>
2020-12-03 22:39:43 +0100

    answer to day 3 part 2

--- python/week2.py
+++ python/week2.py
@@ -13,12 +13,11 @@ puzzle =\
 .#..#...#.#\
 """
 puzzle_split = puzzle.split('\n')
-with open('python/input_2.txt') as puzzle_in: 
+with open('python/input_2.txt') as puzzle_in:
     puzzle_split = [l.strip() for l in puzzle_in.readlines()]
 
 r_max = len(puzzle_split[0])
 d_max = len(puzzle_split)
-
 
 class Vector:
     def __init__(self, x, y):
@@ -38,15 +37,18 @@ def is_tree(pos: Vector) -> int:
 
 if __name__ == "__main__":
     print("hello")
-    step = (3,1)
 
-    cnt = 0
-    pos = Vector(-3,-1)
-    for row in puzzle_split:
-        pos = pos.move(step)
-        hit = is_tree(pos)
-        print(f'row: {row} pos: {pos} hit: {hit}')
-        cnt = cnt + is_tree(pos)
-    
-    print(f'tree cnt: {cnt}')
-    print(f'last postion: {pos}')
+    slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+
+    answer = 1
+    for step in slopes:
+        pos = Vector(0, 0)
+        cnt = 0
+        for x in range(0, len(puzzle_split)-1, step[1]):
+            hit = is_tree(pos)
+            pos = pos.move(step)
+            # print(f'row: {puzzle_split[x]} pos: {pos} hit: {hit}')
+            cnt = cnt + is_tree(pos)
+        answer = answer * cnt
+
+    print(f'answer: {answer}')
